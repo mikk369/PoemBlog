d10aa392147251037c58939b38ba21a1178a05d9
mikk369 <mikk.ereline@vikk.ee>
2022-12-04 13:50:30 +0200

    added protect MW with cookieAuth for limited routes

--- server/routes/poemRoutes.js
+++ server/routes/poemRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const poemApi = require('./../controllers/poemApi');
 const authController = require('./../controllers/authController');
 
-router.route('/').get(poemApi.getAllPoems).post(poemApi.createPoem);
+router
+  .route('/')
+  .get(poemApi.getAllPoems)
+  .post(authController.protect, poemApi.createPoem);
 
 router
   .route('/:id')
-  .get(poemApi.getPoem)
-  .patch(poemApi.updatePoem)
-  .delete(poemApi.deletePoem);
+  .get(authController.protect, poemApi.getPoem)
+  .patch(authController.protect, poemApi.updatePoem)
+  .delete(authController.protect, poemApi.deletePoem);
 
 module.exports = router;

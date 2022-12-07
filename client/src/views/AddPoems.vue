3c6d3f662f4a0bb2ea3e08bb2960f6835790a039
mikk369 <mikk.ereline@vikk.ee>
2022-12-07 13:28:27 +0200

    As user can delete poems[#183952333]

--- client/src/views/AddPoems.vue
+++ client/src/views/AddPoems.vue
@@ -5,33 +5,17 @@
       <div class="addpoem">
         <form>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"
-              >Poem title</label
-            >
-            <input
-              v-model="title"
-              type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
-              placeholder="Poem title"
-            />
+            <label for="exampleFormControlInput1" class="form-label">Poem title</label>
+            <input v-model="title" type="text" class="form-control title-input" id="exampleFormControlInput1"
+              placeholder="Poem title"/>
+            <label for="exampleFormControlInput1" class="poem-author-label">Poem author</label>
+            <input v-model="author" type="text" class="form-control" id="exampleFormControlInput1" placeholder="Poem author"/>
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label"
-              >Insert poem</label
-            >
-            <textarea
-              v-model="text"
-              class="form-control"
-              id="exampleFormControlTextarea1"
-              rows="15"
-            ></textarea>
+            <label for="exampleFormControlTextarea1" class="form-label">Insert poem</label>
+            <textarea v-model="text" class="form-control" id="exampleFormControlTextarea1" rows="15"></textarea>
           </div>
-          <button
-            @click.prevent="addPost()"
-            type="submit"
-            class="btn btn-primary"
-          >
+          <button @click.prevent="addPost()" type="submit" class="btn btn-primary">
             Submit
           </button>
         </form>
@@ -45,12 +29,22 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  padding:70px;
+}
+.title-input{
+  margin-bottom: 3px;
+}
+.poem-author-label{
+  margin-bottom: 8px;
 }
 .addpoem {
   width: 300px;
   justify-content: center;
   align-items: center;
+}
+
+textarea {
+  height: 265px;
 }
 </style>
 <script>
@@ -62,6 +56,7 @@ export default {
   data() {
     return {
       title: '',
+      author:'',
       text: '',
     };
   },
@@ -69,12 +64,14 @@ export default {
     async addPost() {
       await axios.post('http://localhost:3000/api/v1/poems', {
         title: this.title,
+        author: this.author,
         text: this.text,
         headers: {
           Cookie: 'jwt',
         },
       });
       this.title = '';
+      this.author = "",
       this.text = '';
     },
   },
